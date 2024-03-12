b7fb213603a28bd71c4596ca1d5219b85fa78e5c
Selcuk <mizrakli12385@gmail.com>
2024-03-12 23:27:32 +0300

    Day2

--- src/test/java/stepDefination/StepDefinition.java
+++ src/test/java/stepDefination/StepDefinition.java
@@ -12,17 +12,21 @@ public class StepDefinition {
 
     @Given("Database baglantisi kurulur.")
     public void database_baglantisi_kurulur() {
+
         JDBCReusableMethods.getConnection();
+
     }
     @Given("Query01 hazirlanir ve execute edilir.")
     public void query01_hazirlanir_ve_execute_edilir() throws SQLException {
 
         String query01 = "SELECT user_id FROM u168183796_qaloantec.deposits WHERE amount BETWEEN 100 AND 500";
+
         resultSet = JDBCReusableMethods.getStatement().executeQuery(query01);
 
     }
     @Given("ResultSet01 Sonuclar islenir.")
     public void result_set01_sonuclar_islenir() {
+
 
     }
     @Given("Database baglantisi kapatilir.")
